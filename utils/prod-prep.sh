7b0823e9f5df6c0d6f12cc83f6259ed6a8576537
Derrick <derrick.williams@razartech.com>
2023-05-29 08:10:51 -0400

    Update prep script

--- utils/prod-prep.sh
+++ utils/prod-prep.sh
@@ -4,14 +4,17 @@ ROOTDIR=$PWD
 DISTDIR="$ROOTDIR/dist"
 PRODDIR="$ROOTDIR/prod"
 
+
+cd $PWD
+npm run build
+
 echo "cleaning up $PRODDIR"
 rm -rf "$PRODDIR/*"
 
 echo "copying from $DISTDIR"
+mkdir -p $PRODDIR
 cp -R $DISTDIR/* $PRODDIR
 
-cp $ROOTDIR/404.html $PRODDIR
+cp $ROOTDIR/404.html $PRODDIR/404.html
 
-# cleanup old stuff
-# copy dist
-# copy other files
+echo "DONE!"
