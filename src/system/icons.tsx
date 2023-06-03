7947e870c4188a1474c84e95431650b674712c2f
Derrick <derrick.williams@razartech.com>
2023-06-03 08:30:43 -0400

    Adding TallyBrand component

---
+++ src/system/icons.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as TallyFull }  from '../assets/tally-full.svg';
+import { ReactComponent as TallyFullInverse }  from '../assets/tally-full-inverse.svg';
+import { ReactComponent as TallyInitial }  from '../assets/tally-initial.svg';
+import { ReactComponent as TallyInitialInverse }  from '../assets/tally-initial-inverse.svg';
+import { FC } from 'react';
+import { noop } from '../utils';
+
+
+interface TallyBrandProps {
+    full?: boolean;
+    inverse?: boolean;
+    onClick?: () => void;
+}
+export const TallyBrand: FC<TallyBrandProps> = ({ full = false, inverse = false, onClick = noop }: TallyBrandProps) => {
+    const C = full
+        ? (inverse ? TallyFullInverse : TallyFull)
+        : (inverse ? TallyInitialInverse : TallyInitial)
+
+    return <C onClick={onClick} />
+}
