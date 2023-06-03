c92cecf3313286b71c4d52b4ea6b7e9785eeb392
Derrick <derrick.williams@razartech.com>
2023-06-03 08:31:19 -0400

    Renaming to Tally.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import './App.css'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { NavToSettings } from './lib/navButtons'
-import { IoMenu } from 'react-icons/io5'
+import { TallyBrand } from './system/icons';
+import { GrAdd } from 'react-icons/gr'
 import { MenuModal } from './Modal'
 import { useState } from 'react'
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
+  const n = useNavigate();
 
   return (
     <>
       <div className="head-container">
-        <IoMenu className="main-menu" onClick={() => setShowMenu(true) } />
+        <TallyBrand inverse full onClick={() => n('/')} />
+
         <NavToSettings />
       </div>
       <div className="body-container">
         <Outlet />
+      </div>
+      <div className="action-footer">
+        <GrAdd className="main-menu" onClick={() => setShowMenu(true) } />
       </div>
       <MenuModal isOpen={showMenu} onClose={() => setShowMenu(false)} />
     </>
